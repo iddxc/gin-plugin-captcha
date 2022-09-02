de104df06c187356099e800476851456c6b6f29c
George <374744710@qq.com>
2022-09-02 11:19:58 +0800

    修改 服务返回

--- service/captcha_test.go
+++ service/captcha_test.go
@@ -10,9 +10,9 @@ import (
 )
 
 func init() {
-	global.GVA_CONFIG.Redis.Addr = "120.26.196.124:6379"
+	global.GVA_CONFIG.Redis.Addr = "127.0.0.1:6379"
 	global.GVA_CONFIG.Redis.DB = 0
-	global.GVA_CONFIG.Redis.Password = "520qp025"
+	global.GVA_CONFIG.Redis.Password = ""
 	global.GVA_CONFIG.FontFile = "D:\\WorkerPath\\gin-plugin-captcha\\FZSTK.TTF"
 	initialize.Redis()
 }
